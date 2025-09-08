8c7847886a3f141198ce17adc90ac6e85404267b
Prem-Prajapati <premprajapati298@gmail.com>
2025-09-09 00:48:17 +0530

    With 1-D array & operations

--- array1.py
+++ array1.py
@@ -4,4 +4,12 @@ from math import *
 #print(cos(0))
 
 arr = np.array([1,2,3,4])
-print(arr)
+add = arr + 5
+print(add)
+sub = arr - 1
+print(sub)
+mul = arr * 5
+print(mul)
+
+x = np.where(arr % 2 == 0)
+print(x)
