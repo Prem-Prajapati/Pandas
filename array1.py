5323e73ed013c078f64fa0acad760e328e707a22
Prem-Prajapati <premprajapati298@gmail.com>
2025-09-10 22:22:06 +0530

    Added Matrix & it's methods

--- array1.py
+++ array1.py
@@ -5,11 +5,21 @@ from math import *
 
 arr = np.array([1,2,3,4])
 add = arr + 5
-print(add)
+#print(add)
 sub = arr - 1
-print(sub)
+#print(sub)
 mul = arr * 5
-print(mul)
+#print(mul)
 
 x = np.where(arr % 2 == 0)
-print(x)
+#print(x)
+
+A = np.array([[1,2,3],[4,5,6]])
+#print(A.ndim)
+
+#random matrix
+r = np.random.rand(3,3)
+print(r)
+print(r.diagonal())
+print(r.trace())
+print(r.transpose())
