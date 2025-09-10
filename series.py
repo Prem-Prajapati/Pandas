4c97607f0b16ccc5360de6a7e09b53a9875bb3e2
Prem-Prajapati <premprajapati298@gmail.com>
2025-09-11 00:30:06 +0530

    Basic of Series

--- series.py
+++ series.py
@@ -1,1 +1,19 @@
+import numpy as np
 import pandas as pd
+a = pd.Series([1,2,3,4,5])
+print(a)
+
+#with index
+b = pd.Series([1,2,3,4,5],index=['a','b','c','d','e'])
+print(b)
+
+stat_dict = {'Pencil':10,
+             'Eraser':20,
+             'Sharpner':15}
+print(pd.Series(stat_dict))
+
+arr = np.array(['q','w','e','r','t'])
+index = [1,2,3,4,5]
+arr1 = pd.Series(arr,index=index)
+print(arr1)
+print(a.describe())
